2015aae04207274727afd512b3f6a6bfd534a361
alaa-lasoued <drakworldr8@gmail.com>
2020-09-28 18:52:04 +0100

    done

---
+++ client/src/components/logo.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+class Logo extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render() {
+      return <img src="famy-logo.png" width="1320px" id="logofamy"/>;
+    }
+  }
+
+  export default Logo
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,7 @@ import Signup from "./components/singup.jsx"
 import Shop from "./components/shop.jsx"
 import Simulation from "./components/simulation.jsx"
 import NavBar from "./components/navbar.jsx"
-
+import Logo from "./components/logo.jsx"
 class App extends React.Component {
     constructor(props){
         super(props)
@@ -15,11 +15,13 @@ class App extends React.Component {
           displaysignup:false,
           displayshop:false,
           displaySimulation:false,
+          displaylogo:false
         }
     }
     render() {
       return <div>
-          {this.state.displaySimulation?<Signup/>:null}
+          {!this.state.displaylogo?<Logo/>:null}
+          {this.state.displaysignup?<Signup/>:null}
           {this.state.displayshop?<Shop/>:null}
           {this.state.displaylogin?<Login/>:null}
           {this.state.displaySimulation?<Simulation/>:null}
