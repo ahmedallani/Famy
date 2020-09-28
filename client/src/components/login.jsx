f19788d1821fd19a5df815883275b6c55874d17b
alaa-lasoued <drakworldr8@gmail.com>
2020-09-28 17:59:09 +0100

    done

---
+++ client/src/components/login.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+class Login extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render() {
+      return <h1>Bonjour</h1>;
+    }
+  }
+
+  export default Login
---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Navbar extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render() {
+      return <h1>Bonjour</h1>;
+    }
+  }
+
+  export default Navbar
