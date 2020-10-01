1fe2bf22d8eb7625cf56e327be520a6e4d0e88cc
Yassine Knaizia <Yassine.knaizia@esprit.tn>
2020-10-01 13:48:11 +0100

    done

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,34 +1,50 @@
 import React from "react"
 import ReactDOM from 'react-dom'
 import Login from "./components/login.jsx"
-import Signup from "./components/singup.jsx"
+import Signup from "./components/signup.jsx"
 import Shop from "./components/shop.jsx"
 import Simulation from "./components/simulation.jsx"
 import NavBar from "./components/navbar.jsx"
-
+import Logo from "./components/logo.jsx"
+import Signup0 from "./components/signup0.jsx"
 class App extends React.Component {
     constructor(props){
         super(props)
         this.state={
-          balance : 100,
-          Id : 10,
+          ID:"",
+          userdata:{},
           displaynavbar:false,
           displaylogin:false,
           displaysignup:false,
-          displayshop:true,
+          displayshop:false,
           displaySimulation:false,
+          displaylogo:false,
+          displaySignup0:false
         }
+        this.UpdateData=this.UpdateData.bind(this)
+        this.selectCharId=this.selectCharId.bind(this)
+        this.startS=this.startS.bind(this)
+    }
+    UpdateData(data){
+     this.setState({userdata:data})
+    }
+    selectCharId(id){
+      this.setState({ID:id,displaySignup0:true,displaysignup  :false})
+    }
+    startS(){
+      this.setState({displaySimulation:true,displaySignup0:false,displaylogo:false,displaynavbar:true,displaylogin:false})
     }
     render() {
-      return (
-      <div>
-        {this.state.displaySimulation?<Signup/>:null}
-        {this.state.displayshop?<Shop Id={this.state.Id} balance={this.state.balance-this.props.price}/>:null}
-        {this.state.displaylogin?<Login/>:null}
-        {this.state.displaySimulation?<Simulation/>:null}
-        {this.state.displaynavbar?<NavBar/>:null}
-    </div>
-    )};
+      return <div>
+          {this.state.displaynavbar?<NavBar />:null}
+          {this.state.displaylogo?<Logo/>:null}
+          {this.state.displaysignup?<Signup IdS={this.selectCharId}/>:null}
+          {!this.state.displayshop?<Shop id={this.state.ID}/>:null}
+          {this.state.displaylogin?<Login start={this.startS}/>:null}
+          {this.state.displaySimulation?<Simulation/>:null}
+          {this.state.displaySignup0?<Signup0 id={this.state.ID} start={this.startS}/>:null}
+            </div>
+    }
   }
 
   ReactDOM.render(<App/>,document.getElementById("App"))
--- server.js
+++ server.js
@@ -3,46 +3,70 @@ const app = express()
 const port = 3000
 const portS = process.env.PORT || 4001;
 const http = require("http");
-const mongoose = require("mongoose")
+const mongoose=require("mongoose")
 const socketIo = require("socket.io");
+const { random } = require('./Game/game');
+const game=require("./Game/game")
+const dbF=require("./db/schema")
+
+
 
 app.use(express.static(__dirname + '/client/dist'));
 
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://famy:2222@cluster0.m7mng.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb+srv://famy:2222@cluster0.ye5b9.gcp.mongodb.net/famy?retryWrites=true&w=majority", { useNewUrlParser: true, 
+useCreateIndex: true,
+useUnifiedTopology: true 
 });
 
 var db = mongoose.connection;
 
 db.on('error', function() {
-    console.log('mongoose connection error');
+  console.log('mongoose connection error');
 });
 
 db.once('open', function() {
-    console.log('mongoose connected successfully');
+  console.log('mongoose connected successfully');
 });
 
-
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-app.post('/shop',(req,res)=>{
-    console.log(req.body)
-  dbF.updateTokens(req.body.Id,req.body.balance,res)
-  })
+app.post("/register", (req, res) => {
+dbF.registerUser(req.body, res)
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
 
 
+app.post("/position",(req,res)=>{
+  console.log(req.body)
+  res.send()
+})
 
+app.post('/selectChar',(req,res)=>{
+  console.log(req.body)
+dbF.updateskin(req.body.id, req.body.currentskin, res)
+});
+
+app.get('/shop', (req,res) => {
+  dbF.Avatar.find({}, (err,data) => {
+    err ? console.log(err) : res.send(data)
+  });
+});
+
+app.post('/', (req,res) => {
+  dbF.findBalance(req.body.id, res) 
+});
+
+app.post('/',(req,res)=>{
+  console.log(req.body)
+dbF.updateTokens(req.body.id, req.body.balance, res)
+});
 
 //////////////////////Socket Io
 const server = http.createServer(app);
@@ -52,21 +76,50 @@ const io = socketIo(server);
 let interval;
 
 io.on("connection", (socket) => {
-    console.log("New client connected");
-    if (interval) {
-        clearInterval(interval);
-    }
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-    });
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  socket.on("id",data=>{
+    console.log(data)
+  })
+  interval = setInterval(() => getApiAndEmit(socket), 200);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
 const getApiAndEmit = socket => {
-    const response = new Date();
-    // Emitting a new message. Will be consumed by the client
-    socket.emit("FromAPI", response);
+
+  socket.emit("Simulationdata", "test");
 };
 
 server.listen(portS, () => console.log(`Listening on port ${portS}`));
+
+////////////////////////////   Game 
+
+var matrix = Array.from(Array(26), (x) => Array(29).fill(0));
+
+console.table(matrix)
+
+var randomSpawn = function(id,res){
+  var x=game.random("x")
+  var y=game.random("y")
+  if(matrix[x][y]===0){
+     matrix[x][y]=id
+     res.send({place:"done"})
+     console.log()
+  }else{
+    randomSpawn(id,res)
+  }
+  
+}
+
+
+
+
+
+// dbF.id.save((result,err)=>{ // if you want to set the accounts id to 0 uncomment this
+//   console.log(result,err)
+// })
