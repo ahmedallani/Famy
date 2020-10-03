0fdb456fe87ad6005d3b3fb2a45efdc4b9774976
ahmedallani <ahmed.allani@rbk.tn>
2020-10-03 11:39:20 +0100

    Famy-logo.png

--- client/src/components/logo.jsx
+++ client/src/components/logo.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 class Logo extends React.Component {
     render() {
-      return <div> <img src="famy-logo.png" width="53%" id="logofamy"/> </div>
+      // _*_ Famy-logo.png is uppercase
+      return <div> <img src="Famy-logo.png" width="53%" id="logofamy"/> </div>
     }
   }
   export default Logo
